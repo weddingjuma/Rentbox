2816351977a51cc23aa1713594d079bf1f6d1df5
Jussi Viinikka <jussiviinikka@Jussis-MBP.home>
2015-12-22 11:24:21 +0200

    GUIn eka versio

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,35 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('/suunnitelmat/signup.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function searchResults() {
+        View::make('/suunnitelmat/search_results.html');
+    }
+
+    public static function portfolio() {
+        View::make('/suunnitelmat/portfolio.html');
+    }
+
+    public static function unit() {
+        View::make('/suunnitelmat/unit.html');
+    }
+
+    public static function unitEdit() {
+        View::make('/suunnitelmat/unit_edit.html');
+    }
+
+    public static function lease() {
+        View::make('/suunnitelmat/lease.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,25 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/search', function(){
+      HelloWorldController::searchResults();
+  });
+
+    $routes->get('/user/unit', function() {
+    HelloWorldController::unit();
+});
+
+    $routes->get('/user/unit/edit', function() {
+    HelloWorldController::unitEdit();
+});
+
+    $routes->get('/user/unit/lease', function() {
+    HelloWorldController::lease();
+});
+
+
+    $routes->get('/user/portfolio', function() {
+    HelloWorldController::portfolio();
+});
+
